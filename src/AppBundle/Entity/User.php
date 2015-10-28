ed9304f0be1233131f5f580f1c08d3cfefc012d7
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2015-10-28 15:30:00 +0100

    Installation de FOSUserBundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apf
+ * Date: 28/10/15
+ * Time: 13:18
+ */
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends \FOS\UserBundle\Entity\User
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
