0599dbe5e7df00dd74c5e24a78b8eeba32198ade
apflieger <pflieger.arnaud@gmail.com>
2015-12-17 00:56:20 +0100

    Nouvelle task de build du js + watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
+gulp.task('external', function() {
     return gulp.src([
         'node_modules/bootstrap/dist/css/bootstrap.min.css',
         'node_modules/angular/angular.js'
     ]).pipe(gulp.dest('web/build'));
+});
+
+gulp.task('js', function() {
+    return gulp.src([
+        'app/Resources/js/*.js'
+    ]).pipe(gulp.dest('web/build'));
+});
+
+gulp.task('default', ['external', 'js']);
+
+gulp.task('watch', function(){
+    gulp.watch('app/Resources/js/*.js', ['js']);
 });
