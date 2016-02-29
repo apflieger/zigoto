b616fb2801f677af22bcb5726a7d1ac85b9fb6d2
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2016-02-29 21:23:42 +0100

    La prod récupère la connexion à la base via variable d'env pour ne pas mettre les accès sur github. Issue #34

--- app/config/environment.php
+++ app/config/environment.php
@@ -6,4 +6,15 @@
  * Time: 01:19
  */
 
-$container->setParameter('kernel.user', getenv('SYMFONY_USER') ? getenv('SYMFONY_USER') : '');
+function get($key)
+{
+    return getenv($key) ? getenv($key) : '';
+}
+
+$container->setParameter('kernel.user',         get('SYMFONY_USER'));
+
+$container->setParameter('database_host',       get('POSTGRESQL_ADDON_HOST'));
+$container->setParameter('database_port',       get('POSTGRESQL_ADDON_PORT'));
+$container->setParameter('database_user',       get('POSTGRESQL_ADDON_USER'));
+$container->setParameter('database_password',   get('POSTGRESQL_ADDON_PASSWORD'));
+$container->setParameter('database_name',       get('POSTGRESQL_ADDON_DB'));
