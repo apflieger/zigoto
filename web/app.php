db3db4176f6165c4cac2be49ed2418a735a68168
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2016-03-06 20:44:50 +0100

    Unification des points d'entrés dev et prod, les 2 arrivent sur app.php Ca permet d'avoir la même conf apache en local et en prod

--- web/app.php
+++ web/app.php
@@ -1,24 +1,38 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+$env = getenv('SYMFONY_ENV');
+$debug = getenv('SYMFONY_DEBUG');
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-// Enable APC for autoloading to improve performance.
-// You should change the ApcClassLoader first argument to a unique prefix
-// in order to prevent cache key conflicts with other applications
-// also using APC.
-/*
-$apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
-$loader->unregister();
-$apcLoader->register(true);
-*/
+if ($env === 'prod') {
+    if ($_SERVER['HTTP_HOST'] !== 'www.zigotoo.com') {
+        header('HTTP/1.0 403 Forbidden');
+        exit('Environnement de prod sur host '. $_SERVER['HTTP_HOST']. ', c\'est bizarre... Voir ' . __FILE__);
+    }
+
+    // Enable APC for autoloading to improve performance.
+    // You should change the ApcClassLoader first argument to a unique prefix
+    // in order to prevent cache key conflicts with other applications
+    // also using APC.
+    /*
+    $apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
+    $loader->unregister();
+    $apcLoader->register(true);
+    */
+}
+
+if ($debug) {
+    Debug::enable();
+}
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel($env, $debug);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
