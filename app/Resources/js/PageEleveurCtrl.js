7c069635594d06cce748efaa21b21eb9179f33a9
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2016-04-07 01:18:57 +0200

    Styling des boutons des compostants x-editable

--- app/Resources/js/PageEleveurCtrl.js
+++ app/Resources/js/PageEleveurCtrl.js
@@ -1,7 +1,10 @@
-/**
- * Created by arnaudpflieger on 17/12/2015.
- */
 var zigotoo = angular.module('zigotoo', ['xeditable']);
+
+zigotoo.run(function(editableOptions, editableThemes) {
+    // surcharge du template par défaut pour y ajour les styles de btn et les titles
+    editableThemes['default'].submitTpl = '<button type="submit" class="btn btn-primary" title="Ctrl+Entrer">Valider</button>';
+    editableThemes['default'].cancelTpl = '<button type="button btn" class="btn btn-secondary" ng-click="$form.$cancel()" title="Esc">Annuler</button>';
+});
 
 zigotoo.controller('PageEleveurCtrl', ['$scope', '$http', function ($scope, $http) {
 
