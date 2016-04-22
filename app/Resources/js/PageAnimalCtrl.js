19b67b8220b6673712703ca90a6e801af89a7ccd
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2016-04-22 02:29:31 +0200

    Page animal #60 Date de naissance editable

--- app/Resources/js/PageAnimalCtrl.js
+++ app/Resources/js/PageAnimalCtrl.js
@@ -1,6 +1,9 @@
+var moment = require('moment');
+
 module.exports = function($scope, $http) {
     // Variable injectée dans la page par le backend
     $scope.pageAnimal = globPageAnimal;
+    $scope.dateNaissanceString = moment(globPageAnimal.date_naissance).format('DD/MM/YYYY');
 
     $scope.commit = function() {
         $http({
@@ -9,6 +12,7 @@ module.exports = function($scope, $http) {
             data: $scope.pageAnimal
         }).then(function successCallback(response) {
             $scope.pageAnimal = response.data;
+            $scope.dateNaissanceString = moment($scope.pageAnimal.date_naissance).format('DD/MM/YYYY');
         }, function errorCallback(response) {
             console.debug(response);
         });
@@ -17,5 +21,10 @@ module.exports = function($scope, $http) {
     $scope.validateNom = function($nom) {
         if (!$nom)
             return "L'animal doit avoir un nom";
+    };
+
+    $scope.dateNaissanceChanged = function() {
+        $scope.pageAnimal.date_naissance = moment($scope.dateNaissanceString, 'DD-MM-YYYY');
+        $scope.commit();
     };
 };
