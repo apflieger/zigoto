d1d2e75077e8ea6e9f193f85a523f06a72f2e789
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2016-04-23 16:35:39 +0200

    Page animal #60 PageAnimal->statut editable

---
+++ app/Resources/js/ZIntegerModelDirective.js
@@ -0,0 +1,13 @@
+module.exports = function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModel) {
+            ngModel.$parsers.push(function(val) {
+                return parseInt(val, 10);
+            });
+            ngModel.$formatters.push(function(val) {
+                return '' + val;
+            });
+        }
+    };
+};
--- app/Resources/js/zigotoo-editable.js
+++ app/Resources/js/zigotoo-editable.js
@@ -7,3 +7,7 @@ zigotoo.run(['editableOptions', 'editableThemes', require('./ZigotooRun.js')]);
 
 zigotoo.controller('PageEleveurCtrl', ['$scope', '$http', require('./PageEleveurCtrl.js')]);
 zigotoo.controller('PageAnimalCtrl', ['$scope', '$http', require('./PageAnimalCtrl.js')]);
+
+// Directive qui sert à typer le ngModel en integer
+// Voir https://docs.angularjs.org/api/ng/directive/select
+zigotoo.directive('zIntegerModel', require('./ZIntegerModelDirective.js'));
