56db310ef319f98b74b04057a101112b46d3eb8c
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2016-04-25 16:37:07 +0200

    Upload de photos sur la page animal #60 Commit déclenché coté client après un upload d'images
    Gestion de plusieurs fin d'upload qui génère plusieurs commits.
    On a un état de changements en pending qui déclenche un 2eme commuit à la réponse
    du premier pour que les commits soient fast forward

--- app/Resources/js/PageAnimalCtrl.js
+++ app/Resources/js/PageAnimalCtrl.js
@@ -4,16 +4,47 @@ module.exports = function($scope, $http, Upload) {
     // Variable injectée dans la page par le backend
     $scope.pageAnimal = globPageAnimal;
     $scope.dateNaissanceString = moment(globPageAnimal.date_naissance).format('DD/MM/YYYY');
+    $scope.dirtyPhotos = globPageAnimal.photos || [];
+
+    // si une requete http de commit est en cours
+    var commiting = false;
+
+    /*
+     si commit() a été appelé alors qu'une requete de commit est deja en cours,
+     on ne peut pas en lancer une 2eme en parallèle car le 2eme commit
+     ne serait pas fastforward du 1er. On bloque alors le 2eme commit, en attendant
+     que le 1er se finisse. C'est donc à la réponse du 1er commit que le 2eme est lancé.
+     */
+    var pendingChanges = false;
 
     $scope.commit = function() {
+
+        if (commiting) {
+            pendingChanges = true;
+            return;
+        }
+        commiting = true;
         $http({
             method: 'POST',
             url: '/animal/' + $scope.pageAnimal.id,
             data: $scope.pageAnimal
         }).then(function successCallback(response) {
-            $scope.pageAnimal = response.data;
+            commiting = false;
+            if (pendingChanges) {
+                $scope.pageAnimal.head = response.data.head;
+            } else {
+                $scope.pageAnimal = response.data;
+            }
             $scope.dateNaissanceString = moment($scope.pageAnimal.date_naissance).format('DD/MM/YYYY');
+
+            if (pendingChanges) {
+                pendingChanges = false;
+                $scope.commit();
+            }
+
         }, function errorCallback(response) {
+            commiting = false;
+            pendingChanges = true;
             console.debug(response);
         });
     };
@@ -40,19 +71,13 @@ module.exports = function($scope, $http, Upload) {
             for (var i = 0; i < $newFiles.length; i++) {
                 var file = $newFiles[i];
 
-                $scope.pageAnimal.photos = $scope.pageAnimal.photos || [];
+                file.nom = file.name;
+                file.uploaded = false;
 
-                var photo = {
-                    file: file,
-                    uploaded: false
-                };
-
-                $scope.pageAnimal.photos.push(photo);
-
-                (function(photo) {
+                (function(file) {
                     // Il n'y a aucune authentification, le bucket autorise tous les POST.
                     Upload.upload({
-                        url: 'https://zigotoo-runtime.s3.amazonaws.com/', //S3 upload url including bucket name
+                        url: 'https://zigotoo-runtime.s3.amazonaws.com/',
                         method: 'POST',
                         data: {
                             key: 'images/' + file.name,
@@ -60,9 +85,13 @@ module.exports = function($scope, $http, Upload) {
                             file: file
                         }
                     }).then(function(response) {
-                        photo.uploaded = true;
+                        file.uploaded = true;
+                        $scope.pageAnimal.photos = $scope.dirtyPhotos.filter(function(photo){
+                            return photo.uploaded
+                        });
+                        $scope.commit();
                     });
-                })(photo);
+                })(file);
             }
         }
     };
