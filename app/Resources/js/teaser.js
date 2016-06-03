144006afedbefe68710f99951dc08ad64af11a10
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2016-06-04 00:17:01 +0200

    Home teaser : Ajout d'une carte en 1er layer

--- app/Resources/js/teaser.js
+++ app/Resources/js/teaser.js
@@ -1,6 +1,23 @@
-/**
- * Created by apflieger on 22/02/16.
- */
 $(".layer-arrow").click(function() {
     $.scrollTo($(this).parent(), 650);
 });
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 46.227, lng: 2.213},
+        zoom: 6,
+        mapTypeControl: false,
+        streetViewControl: false
+    });
+
+    /*var geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({address: "France"}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });*/
+}
